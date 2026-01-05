b833f5b65e49c6587b5b20095e58aaa0944b91a7
FunFR <1670065+FunFR@users.noreply.github.com>
2026-01-05 11:29:44 +0100

    fix: remove controls feature temporarily

--- custom_components/indygo_pool/__init__.py
+++ custom_components/indygo_pool/__init__.py
@@ -14,8 +14,6 @@ from .coordinator import IndygoPoolDataUpdateCoordinator
 PLATFORMS: list[Platform] = [
     Platform.SENSOR,
     Platform.BINARY_SENSOR,
-    Platform.SWITCH,
-    Platform.SELECT,
 ]
 
 
